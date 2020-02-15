fc4e92847b3ba878dfdf9be33cec9af5ce45126c
ChoonKiat <choonkiat.dynp@gmail.com>
2020-02-15 10:45:16 -0500

    removed mongoDB backend and added google firestore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from './components/home.js';
 import User from './components/user.js';
 import Counselor from './components/counselor.js';
+import Journal from './components/journal.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/user" component={User} />
         <Route exact path="/counselor" component={Counselor} />
+        <Route exact path="/journal" component={Journal} />
       </Switch>
     </Router>
   );
