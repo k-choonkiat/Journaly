19d28e9f29cd7775c204b1c87d0946b54ac6f6da
ChoonKiat <choonkiat.dynp@gmail.com>
2020-02-15 03:19:09 -0500

    added user and counselor models

--- frontend/src/components/counselor.js
+++ frontend/src/components/counselor.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default function Counselor(){
   return(
     <div>
       <h1>Hello, Counselor</h1> 
+      <Link to="/">Home</Link>
     </div>
   )
 }
