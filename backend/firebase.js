5e26a77fdbd8aad5d4425a736179e8f3b0f07dd6
ChoonKiat <choonkiat.dynp@gmail.com>
2020-02-15 09:57:15 -0500

    add firebase

---
+++ backend/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCrKMvV5lrJp_g-Qa5w1L98Xv_37-EhXD0",
+    authDomain: "online-journal-18782.firebaseapp.com",
+    databaseURL: "https://online-journal-18782.firebaseio.com",
+    projectId: "online-journal-18782",
+    storageBucket: "online-journal-18782.appspot.com",
+    messagingSenderId: "347187722885",
+    appId: "1:347187722885:web:ca25aa65e6eacf4ca247ec",
+    measurementId: "G-F0LS1YLJHH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
