eab60b73af984a8f7c7558dd55decd3033fec6be
Xiangmin Mo <xiangminmo6121@gmail.com>
2020-02-16 05:53:51 -0500

    css for journal-log

--- frontend/src/components/counselor.js
+++ frontend/src/components/counselor.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+<<<<<<< HEAD
 import c from '../images/c.jpeg'
 import '../styling/counselor.css'
 import '../index.css'
@@ -13,5 +14,17 @@ export default function Counselor(){
         <Link to="/">Home</Link> 
       </div>
     </body>
+=======
+import Nav from './nav'
+
+export default function Counselor(){
+  return(
+    <div>
+      <Nav/>
+      <h1>Hello, Counselor</h1> 
+      <Link to="/">Home</Link>
+
+    </div>
+>>>>>>> master
   )
 }
