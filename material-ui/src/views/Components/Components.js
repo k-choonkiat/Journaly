fe2d0c75d8d8d8162fb3240fbbbde383f0d259da
[Divya Sri Thala] <[divyasri.thala@stonybrook.edu]>
2020-02-16 02:46:53 -0500

    finished counselor's page

--- material-ui/src/views/Components/Components.js
+++ material-ui/src/views/Components/Components.js
@@ -7,6 +7,22 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
+
+
+// import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+ import MenuItem from '@material-ui/core/MenuItem';
+// import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+
+
 // import FormLabel from '@material-ui/core/FormLabel';
 // import FormControl from '@material-ui/core/FormControl';
 // import FormGroup from '@material-ui/core/FormGroup';
@@ -29,7 +45,7 @@ import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
+// import Button from "components/CustomButtons/Button.js";
 import Parallax from "components/Parallax/Parallax.js";
 // sections for this page
 //import { makeStyles } from '@material-ui/core/styles';
@@ -55,11 +71,11 @@ import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
 import SectionLogin from "./Sections/SectionLogin.js";
 import SectionExamples from "./Sections/SectionExamples.js";
 import SectionDownload from "./Sections/SectionDownload.js";
-import doctor1 from "../../assets/img/faces/a.jpg"
-import doctor2 from "../../assets/img/faces/b.jpg"
-import doctor3 from "../../assets/img/faces/c.jpg"
-import doctor4 from "../../assets/img/faces/d.jpg"
-import doctor5 from "../../assets/img/faces/e.jpg"
+import doctor1 from "../../assets/img/faces/c2.jpeg"
+import doctor2 from "../../assets/img/faces/c1.jpeg"
+import doctor3 from "../../assets/img/faces/c3.jpeg"
+import doctor4 from "../../assets/img/faces/c4.jpeg"
+import doctor5 from "../../assets/img/faces/c5.jpeg"
 
 import styles from "assets/jss/material-kit-react/views/components.js";
 
@@ -125,6 +141,61 @@ function StyledRadio(props) {
 export default function Components(props) {
   const classes = useStyles();
   const { ...rest } = props;
+
+
+  // export default function DialogSelect() {
+  //   const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false);
+    const [open3, setOpen3] = React.useState(false);
+    const [open4, setOpen4] = React.useState(false);
+    const [open5, setOpen5] = React.useState(false);
+  
+    const handleClickOpen = () => {
+       setOpen(true);
+     };
+
+     const handleClickOpen2 = () => {
+      setOpen2(true);
+    };
+
+    const handleClickOpen3 = () => {
+      setOpen3(true);
+    };
+
+    const handleClickOpen4 = () => {
+      setOpen4(true);
+    };
+
+    const handleClickOpen5 = () => {
+      setOpen5(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+     const handleClose2 = () => {
+       setOpen2(false);
+     };
+
+     const handleClose3 = () => {
+      setOpen3(false);
+    };
+
+    const handleClose4 = () => {
+      setOpen4(false);
+    };
+
+    const handleClose5 = () => {
+      setOpen5(false);
+    };
+
+
+
+
+
+
   return (
     <div>
       {/* <Header
@@ -204,6 +275,7 @@ export default function Components(props) {
                     tabIcon: "personPinIcon",
                     tabContent: (
                       <div>
+                        <div>
                       <Card className={classes.root}>
                       <CardActionArea>
                         < CardMedia
@@ -226,11 +298,28 @@ export default function Components(props) {
                           Share
                         </Button>
                         <Button size="small" color="primary">
-                          Learn More
+                          
+
+
+                        <Button onClick={handleClickOpen}>Contact</Button>
+      <Dialog className= "1" disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
+        <DialogTitle  style={{textAlign: "center"}}>Contact No</DialogTitle>
+        <h5 style={{textAlign: "center", fontWeight: "bold"}}>6755678945</h5>
+        <DialogTitle style={{textAlign: "center"}}>Email ID</DialogTitle> 
+        <h5 style={{textAlign: "center", fontSize: "0.80em", fontWeight: "bold"}}>mary.jonas@gmail.com</h5>       
+    
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
                         </Button>
                       </CardActions>
                     </Card>
-                    
+                    </div>
+
+                      <div>
                     <Card className={classes.root}>
                       <CardActionArea>
                         < CardMedia
@@ -239,7 +328,7 @@ export default function Components(props) {
                           title=  "Contemplative Reptile"
                         />
                         <CardContent>
-                          <img style= {{height: "200px", width: "180px"}} src= {doctor2} alt= "doctorB" />
+                          <img style= {{height: "200px", width: "280px"}} src= {doctor2} alt= "doctorB" />
                           <Typography gutterBottom variant="h5" component="h2">
                             Miachel Kamphff
                           </Typography>
@@ -253,13 +342,26 @@ export default function Components(props) {
                           Share
                         </Button>
                         <Button size="small" color="primary">
-                          Learn More
+                          <Button onClick={handleClickOpen2}>Contact</Button>
+      <Dialog className= "2" disableBackdropClick disableEscapeKeyDown open={open2} onClose={handleClose2}>
+        <DialogTitle style={{textAlign: "center"}}>Contact No</DialogTitle>
+        <h5 style={{textAlign: "center", fontWeight: "bold"}}>7645626363</h5>
+        <DialogTitle style={{textAlign: "center"}}>Email ID</DialogTitle> 
+        <h5 style={{textAlign: "center", fontSize: "0.80em", fontWeight: "bold"}}>miachel.kamphff@gmail.com</h5>       
+        <DialogActions>
+          <Button onClick={handleClose2} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
                         </Button>
                       </CardActions>
                     </Card>
+                      </div>
+                        
 
 
-
+                      <div>
                     <Card className={classes.root}>
                       <CardActionArea>
                         < CardMedia
@@ -268,7 +370,7 @@ export default function Components(props) {
                           title=  "Contemplative Reptile"
                         />
                         <CardContent>
-                          <img style= {{height: "200px", width: "180px"}} src= {doctor3} alt= "doctorC" />
+                          <img style= {{height: "200px", width: "280px"}} src= {doctor3} alt= "doctorC" />
                           <Typography gutterBottom variant="h5" component="h2">
                             Patric Star
                           </Typography>
@@ -282,13 +384,26 @@ export default function Components(props) {
                           Share
                         </Button>
                         <Button size="small" color="primary">
-                          Learn More
+                        <Button onClick={handleClickOpen3}>Contact</Button>
+      <Dialog className= "3" disableBackdropClick disableEscapeKeyDown open={open3} onClose={handleClose3}>
+        <DialogTitle style={{textAlign: "center"}}>Contact No</DialogTitle>
+        <h5 style={{textAlign: "center", fontWeight: "bold"}}>5436467488</h5>
+        <DialogTitle style={{textAlign: "center"}}>Email ID</DialogTitle> 
+        <h5 style={{textAlign: "center", fontSize: "0.80em", fontWeight: "bold"}}>patric.star@gmail.com</h5>       
+
+        <DialogActions>
+          <Button onClick={handleClose3} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
                         </Button>
                       </CardActions>
                     </Card>
+                    </div>
 
 
-
+                      <div>
                     <Card className={classes.root}>
                       <CardActionArea>
                         < CardMedia
@@ -297,9 +412,9 @@ export default function Components(props) {
                           title=  "Contemplative Reptile"
                         />
                         <CardContent>
-                          <img style= {{height: "200px", width: "250px"}} src= {doctor4} alt= "doctorD" />
+                          <img style= {{height: "200px", width: "280px"}} src= {doctor4} alt= "doctorD" />
                           <Typography gutterBottom variant="h5" component="h2">
-                            Mira klein
+                            joseph clain
                           </Typography>
                           <Typography variant="body2" color="textSecondary" component="p">
                           When you come for cognitive behaviour therapy, the anxiety and depression that you come forward to work on is addressed by looking at two main aspects - cognitions first, then the behaviours. What are cognitions? The definition of cognition is "the...
@@ -311,14 +426,25 @@ export default function Components(props) {
                           Share
                         </Button>
                         <Button size="small" color="primary">
-                          Learn More
+                        <Button onClick={handleClickOpen4}>Contact</Button>
+      <Dialog className= "4" disableBackdropClick disableEscapeKeyDown open={open4} onClose={handleClose4}>
+        <DialogTitle style={{textAlign: "center"}}>Contact No</DialogTitle>
+        <h5 style={{textAlign: "center", fontWeight: "bold"}}>4467747478</h5>
+        <DialogTitle style={{textAlign: "center"}}>Email ID</DialogTitle> 
+        <h5 style={{textAlign: "center", fontSize: "0.80em", fontWeight: "bold"}}>joseph.clain@gmail.com</h5>       
+        <DialogActions>
+          <Button onClick={handleClose4} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
                         </Button>
                       </CardActions>
                     </Card>
+                    </div>
 
 
-
-
+                    <div>
                     <Card className={classes.root}>
                       <CardActionArea>
                         < CardMedia
@@ -327,9 +453,9 @@ export default function Components(props) {
                           title=  "Contemplative Reptile"
                         />
                         <CardContent>
-                          <img style= {{height: "200px", width: "180px"}} src= {doctor5} alt= "doctorE" />
+                          <img style= {{height: "200px", width: "280px"}} src= {doctor5} alt= "doctorE" />
                           <Typography gutterBottom variant="h5" component="h2">
-                            Mariam sen
+                            Mira kathy
                           </Typography>
                           <Typography variant="body2" color="textSecondary" component="p">
                           Anxiety can affect all our lives in so many ways, and it's certainly an issue that is being explored more frequently in the therapy room. All of us can feel anxious, and we can often effectively manage these complex emotions, but sometimes our...
@@ -341,10 +467,22 @@ export default function Components(props) {
                           Share
                         </Button>
                         <Button size="small" color="primary">
-                          Learn More
+                        <Button onClick={handleClickOpen5}>Contact</Button>
+      <Dialog className= "5" disableBackdropClick disableEscapeKeyDown open={open5} onClose={handleClose5}>
+        <DialogTitle style={{textAlign: "center"}}>Contact No</DialogTitle>
+        <h5 style={{textAlign: "center", fontWeight: "bold"}}>6476545660</h5>
+        <DialogTitle style={{textAlign: "center"}}>Email ID</DialogTitle> 
+        <h5 style={{textAlign: "center", fontSize: "0.80em", fontWeight: "bold"}}>mira.kathy@gmail.com</h5>       
+        <DialogActions>
+          <Button onClick={handleClose5} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
                         </Button>
                       </CardActions>
                     </Card>
+                    </div>
                     </div>
 
                     ),
