dea06a33f1d6ad74a2b41db83b1aa5e60c8680d9
[Divya Sri Thala] <[divyasri.thala@stonybrook.edu]>
2020-02-16 04:49:39 -0500

    I have added a review button

--- material-ui/src/views/Components/Components.js
+++ material-ui/src/views/Components/Components.js
@@ -70,6 +70,75 @@ import doctor3 from "../../assets/img/faces/con4.jpg"
 import doctor4 from "../../assets/img/faces/con5.jpg"
 import doctor5 from "../../assets/img/faces/con3.png"
 
+
+
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Rating from 'material-ui-rating'
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
+//import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+
+
+
+
+
+const StyledRating = withStyles({
+  iconFilled: {
+    color: '#ff6d75',
+  },
+  iconHover: {
+    color: '#ff3d47',
+  },
+})(Rating);
+
+const customIcons = {
+  1: {
+    icon: <SentimentVeryDissatisfiedIcon />,
+    label: 'Very Dissatisfied',
+  },
+  2: {
+    icon: <SentimentDissatisfiedIcon />,
+    label: 'Dissatisfied',
+  },
+  3: {
+    icon: <SentimentSatisfiedIcon />,
+    label: 'Neutral',
+  },
+  4: {
+    icon: <SentimentSatisfiedAltIcon />,
+    label: 'Satisfied',
+  },
+  5: {
+    icon: <SentimentVerySatisfiedIcon />,
+    label: 'Very Satisfied',
+  },
+};
+
+function IconContainer(props) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+IconContainer.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+
+
+
+
+
+
+
 const useStyles = makeStyles({
   root: {
     '&:hover': {
@@ -449,6 +518,28 @@ export default function Components(props) {
 
       </GridContainer>
 
+
+
+      
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">Custom icon and color</Typography>
+        <StyledRating
+          name="customized-color"
+          defaultValue={2}
+          getLabelText={value => `${value} Heart${value !== 1 ? 's' : ''}`}
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
+        />
+      </Box>
+
+
     </div>
   );
 }
+
+
+
+
+
+
+
